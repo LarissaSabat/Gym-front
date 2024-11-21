0b2fe77f2653d3b120c91916159e35e2003110fc
Sabat <larissasbc@hotmail.com>
2024-11-21 08:01:00 -0300

    Gym-frontend

---
+++ src/app/app.component.ts
@@ -0,0 +1,17 @@
+import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+import { CabecalhoComponent } from "./componentes/cabecalho/cabecalho.component";
+import { FooterComponent } from "./componentes/footer/footer.component";
+import { CriarContratosComponent } from "./componentes/contratos/criar-contratos/criar-contratos.component";
+import { MatriculaComponent } from "./componentes/contratos/matricula/matricula.component";
+
+@Component({
+  selector: 'app-root',
+  standalone: true,
+  imports: [RouterOutlet, CabecalhoComponent, FooterComponent, CriarContratosComponent, MatriculaComponent],
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.css'
+})
+export class AppComponent {
+  title = 'gym-front';
+}
---
+++ src/app/componentes/contratos/criar-contratos/criar-contratos.component.ts
@@ -0,0 +1,42 @@
+import { ContratosService } from './../../service/contratos.service';
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+
+@Component({
+  selector: 'app-criar-contratos',
+  standalone: true,
+  imports: [],
+  templateUrl: './criar-contratos.component.html',
+  styleUrl: './criar-contratos.component.css'
+})
+export class CriarContratosComponent implements OnInit {
+  contrato = {
+    id: '',
+    nome:'',
+    email:'',
+    telefone: '',
+  }
+  router: any;
+
+  constructor( private contratosService: ContratosService ) { }
+
+  ngOnInit(): void {
+  }
+
+  criarContrato (){
+    this.contratosService.criarContrato(this.contrato).subscribe(
+      response => {
+        console.log('Contrato criado com sucesso:', response);
+        
+      },
+      error => {
+        console.error('Erro ao criar contrato:', error);
+      }
+    );
+  }
+
+  cancelar() {
+    this.router.navigate(['/home'])
+  }
+
+}
